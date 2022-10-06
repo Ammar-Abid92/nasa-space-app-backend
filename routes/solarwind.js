5091818f8a4a384fd89e52c46352bcb3a722bbc4
Ammar-Abid92 <72232879+Ammar-Abid92@users.noreply.github.com>
2022-10-06 15:03:26 +0500

    Remove redundant code

--- routes/solarwind.js
+++ routes/solarwind.js
@@ -2,26 +2,26 @@ const express = require('express')
 const axios = require('axios')
 const router = express.Router()
 
-router.get('/all', async (request, response) => {
-  axios.get('https://services.swpc.noaa.gov/products/solar-wind/mag-2-hour.json')
-    .then(res => {
-      // [ 'time_tag', 'density', 'speed', 'temperature' ]
-      // let keys = data[0]
-      res.data.shift()
-      let jsonData = res.data.map(x => ({
-        time_tag: x[0],
-        density: x[1],
-        speed: x[2],
-        temperature: x[3]
-      }))
+// router.get('/all', async (request, response) => {
+//   axios.get('https://services.swpc.noaa.gov/products/solar-wind/mag-2-hour.json')
+//     .then(res => {
+//       // [ 'time_tag', 'density', 'speed', 'temperature' ]
+//       // let keys = data[0]
+//       res.data.shift()
+//       let jsonData = res.data.map(x => ({
+//         time_tag: x[0],
+//         density: x[1],
+//         speed: x[2],
+//         temperature: x[3]
+//       }))
 
-      response.status(200).json({
-        message: "Data fetched",
-        data: jsonData
+//       response.status(200).json({
+//         message: "Data fetched",
+//         data: jsonData
 
-      })
-    })
-})
+//       })
+//     })
+// })
 
 router.get('/7days', async (request, response) => {
 
